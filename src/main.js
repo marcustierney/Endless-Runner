800ecd9c2ac87718e588508243f26fabb5af243e
Marcus <mstierne@ucsc.edu>
2025-01-29 21:35:03 -0800

    starting point

--- src/main.js
+++ src/main.js
@@ -12,4 +12,4 @@ let borderUISize = game.config.height / 15
 let borderPadding = borderUISize / 3
 
 // reserve keyboard bindings
-let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT, keySPACE
---
+++ src/scenes/Menu.js
@@ -0,0 +1,43 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene")
+    }
+
+    preload() {
+        // load images/tile sprites
+      
+        // load audio
+        this.load.audio('sfx-select', './assets/sfx-select.wav')
+    }
+    
+
+    create() {
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        // display menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'COW DAY', menuConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/2, 'Press SPACEBAR To Start', menuConfig).setOrigin(0.5)
+        menuConfig.backgroundColor = '#00FF00'
+        menuConfig.color = '#000'
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)       
+    }
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+          // play
+          this.scene.start('playScene')    
+        }
+      }
+}
